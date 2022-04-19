27606480257fbf796278bfd0062a653cd9104a2a
t-eviatarz <t-eviatarz@microsoft.com>
2022-04-19 12:29:45 +0300

    - now parsing number from highlited context

--- background.js
+++ background.js
@@ -5,3 +5,18 @@ chrome.runtime.onInstalled.addListener(function() {
       "contexts": ["selection"]
     });
   });
+
+chrome.contextMenus.onClicked.addListener(function (info, tab) {
+  if (tab) {
+    if (info.menuItemId === "messageContextMenu") {
+      console.log({"info": info, "tab": tab});
+      number = parseNumber(info.selectionText);
+      console.log("parsed number is: " + number);
+    }
+  }
+});
+
+function parseNumber(text) {
+  number = text.match(/\+?\d+/);
+  return number;
+}
